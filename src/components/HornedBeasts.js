470594ae4dde3ed9a1dc9f012d495dc08e534d9e
Ala Abu Rish <alaa.rish2@gmail.com>
2021-08-16 20:37:49 +0300

    lab1

---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+
+    render() {
+
+        return (
+
+            <>
+            <section>
+                <h2>{this.props.title}</h2>
+
+                <img src={this.props.imageUrl} alt="horned" title="horned" />
+
+                <p>{this.props.description}</p>
+            </section>
+            </>
+
+        );
+    }
+}
+
+export default HornedBeasts;
