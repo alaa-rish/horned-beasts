3ab51db960af1ee2df6fc62605f06595401b1da8
Ala Abu Rish <alaa.rish2@gmail.com>
2021-08-16 21:35:37 +0300

    Lab2

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,20 +1,36 @@
 
 import React from 'react';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 class HornedBeasts extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          votes: 0,
+        };
+      }
+
+      increaseVotes = () => {
+        this.setState({
+            votes: this.state.votes + 1
+        })
+    }
 
     render() {
 
         return (
 
             <>
-            <section>
-                <h2>{this.props.title}</h2>
-
-                <img src={this.props.imageUrl} alt="horned" title="horned" />
-
-                <p>{this.props.description}</p>
-            </section>
+            <Card style={{ width: '18rem' }}>
+                <Card.Title>{this.props.title}</Card.Title>
+                <Card.Img variant="top" src={this.props.imageUrl} style={{width: '300px'}}></Card.Img>
+                <Card.Body></Card.Body>
+                <Card.Text>{this.props.description}</Card.Text>
+                <Card.Text>{this.state.votes} ♥</Card.Text>
+                <Button onClick={this.increaseVotes}>Vote</Button>
+            </Card>
             </>
 
         );
